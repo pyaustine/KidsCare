c8ad15e162795a65b11fcfd8cd9264c1e1d79b80
sphinx-austin <austinjuniour4@gmail.com>
2023-11-14 21:05:49 +0300

    register model

--- autismApp/admin.py
+++ autismApp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from . models import Result
 
 # Register your models here.
+
+class DataAdmin(admin.ModelAdmin):
+    list_display = ('age',
+                    'sex',
+                    'a1',
+                    'a2',
+                    'a3',
+                    'a4',
+                    'a5',
+                    'a6',
+                    'a7',
+                    'a8',
+                    'a9',
+                    'a10',
+                    'jaundice',
+                    'asd_history',
+                    'test_completed_by',
+                    'outcome',
+                    'date'
+                    )
+
+admin.site.register(Result, DataAdmin)
