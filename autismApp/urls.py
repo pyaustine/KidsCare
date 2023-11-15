3f6445a7283145cfb15cf7812b5cfa8d1b5e9e02
sphinx-austin <austinjuniour4@gmail.com>
2023-11-15 09:55:10 +0300

    update

--- autismApp/urls.py
+++ autismApp/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.home, name='home'),
     path('welcome/', views.welcome, name='welcome'),
-    path('privacy/', views.privacy_policy, name='privacy_policy'),
-    path('terms/', views.terms_of_use, name='terms_of_use'),
+    path('privacy/', views.privacy_policy, name='privacy'),
+    path('terms/', views.terms_of_use, name='terms'),
     path('survey/', views.survey, name='survey'),
     path('survey/predictor/', views.predictor, name='predictor'),
 ]
