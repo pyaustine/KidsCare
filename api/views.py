a56f2e4378e422ecce03ff325746a9e3b76df2e9
sphinx-austin <austinjuniour4@gmail.com>
2023-11-21 08:29:54 +0300

    test api view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def test(request):
+    data = {
+        'id' : 12,
+        'name' : 'Austine',
+        'account_number' : 987194712 
+    }
+
+    return render(request, 'api/test.html', {'data': data})
