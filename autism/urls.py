00ea31181c4f30734c424fdb36d4bb78b0d8641e
sphinx-austin <austinjuniour4@gmail.com>
2023-11-21 21:58:45 +0300

    fix api route

--- autism/urls.py
+++ autism/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('autismApp.urls')),
-    path('api', include('api.urls')),
+    path('api/', include('api.urls')),
 ]
