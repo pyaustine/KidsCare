9fb44479c65ddaf8b8f2167e3bd0d2470740f5ad
sphinx-austin <austinjuniour4@gmail.com>
2023-12-07 09:14:58 +0300

    add admin route(Soft UI)

--- autism/urls.py
+++ autism/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # path('', include('admin_soft.urls')),
     path('admin/', admin.site.urls),
     path('', include('autismApp.urls')),
     path('api/', include('api.urls')),
